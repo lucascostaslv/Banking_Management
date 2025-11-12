1eab6c8ba45cb0640a991b73aa021882e90ec14b
Luís Fernando Ventura Ferreira <venturaluisf7@outlook.com>
2025-11-12 19:50:10 -0300

    Update DB-Structure.sql
    Created table userGroup.
    Added condition on delete and on update on every foreign key.
    Created some foreign keys that couldn't be created before because the tables referenced dind't exist yet.
    Changed the order of tables' creation so foreign keys don't reference tables that weren't created.

--- Implementation/Database/DB-Structure.sql
+++ Implementation/Database/DB-Structure.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE IF NOT EXISTS payment_bank;
-
 USE payment_bank;
 
 CREATE TABLE tb_user (
@@ -10,7 +9,15 @@ CREATE TABLE tb_user (
     birth_day DATE NOT NULL
 );
 
-#Adicionar aqui grupo de usuários
+CREATE TABLE tb_userGroup (
+	id VARCHAR(9) PRIMARY KEY,
+	type INT NOT NULL, 
+
+	CONSTRAINT FK_user_in_userGroup
+	FOREIGN KEY (id) REFERENCES tb_user (id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
+);
 
 CREATE TABLE tb_client (
 	id VARCHAR(9) PRIMARY KEY,
@@ -18,16 +25,19 @@ CREATE TABLE tb_client (
     state VARCHAR(2),
     
     CONSTRAINT FK_user_client
-    FOREIGN KEY (id) REFERENCES tb_user(id)
-    #accounts - fazer uma tabela exclusiva para referenciar depois 
+    FOREIGN KEY (id) REFERENCES tb_user (id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
 );
 
 CREATE TABLE tb_manager (
 	id VARCHAR(9) PRIMARY KEY,
 	role VARCHAR(255),
     
-    CONSTRAINT FK_user_client
-    FOREIGN KEY (id) REFERENCES tb_user(id)
+    CONSTRAINT FK_user_manager
+    FOREIGN KEY (id) REFERENCES tb_user (id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
 );
 
 CREATE TABLE tb_account (
@@ -36,30 +46,42 @@ CREATE TABLE tb_account (
     balance DECIMAL(13,2) NOT NULL DEFAULT 0.0,
 	type VARCHAR(63) NOT NULL,
     open_date TIMESTAMP NOT NULL,
-    
-    pix_keys VARCHAR(10), #Adicionar Foreing key para a tabela depois
+    pix_keys VARCHAR(10),
     client_id VARCHAR(9),
-    
+
     CONSTRAINT FK_account_client
     FOREIGN KEY (client_id) REFERENCES tb_client (id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
 );
 
 CREATE TABLE tb_pixKey (
 	id VARCHAR(10) PRIMARY KEY,
     email VARCHAR(127) UNIQUE,
-    phone_number VARCHAR(14), #+5500900000000
+    phone_number VARCHAR(14), -- +5500900000000
     rand_key VARCHAR(63),
     
     CONSTRAINT FK_pixKey_account 
     FOREIGN KEY (id) REFERENCES tb_account (id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
 );
+
+-- adiciona FK que referencia tb_pixKey
+ALTER TABLE tb_account
+ADD CONSTRAINT FK_account_pixKey
+FOREIGN KEY (pix_keys) REFERENCES tb_pixKey (id)
+	ON UPDATE CASCADE  
+    ON DELETE RESTRICT;
 
 CREATE TABLE tb_current (
 	id VARCHAR(10) PRIMARY KEY,
     monthly_tax DECIMAL(4,2) NOT NULL DEFAULT 0.52,
     
-    CONSTRAINT FK_pixKey_account 
+    CONSTRAINT FK_current_account 
     FOREIGN KEY (id) REFERENCES tb_account (id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
 );
 
 CREATE TABLE tb_savings (
@@ -67,24 +89,30 @@ CREATE TABLE tb_savings (
     return_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     return_amount DECIMAL(9,2) NOT NULL,
     
-    CONSTRAINT FK_pixKey_account 
+    CONSTRAINT FK_savings_account 
     FOREIGN KEY (id) REFERENCES tb_account (id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
 );
 
 CREATE TABLE tb_transaction (
-	id VARCHAR(17) PRIMARY KEY, #"ddyyyyMMHHmmssSSS"
+	id VARCHAR(17) PRIMARY KEY, -- "ddyyyyMMHHmmssSSS"
     type VARCHAR(63) NOT NULL,
     payment_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     transaction_value DECIMAL(13,2) NOT NULL,
     
-    origin_account_id VARCHAR(10) ,
+    origin_account_id VARCHAR(10),
     target_account_id VARCHAR(10) NOT NULL,
     
     CONSTRAINT FK_transaction_OGaccount 
-    FOREIGN KEY (origin_account_id) REFERENCES tb_account (id),
+    FOREIGN KEY (origin_account_id) REFERENCES tb_account (id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT,
     
     CONSTRAINT FK_transaction_TGaccount 
     FOREIGN KEY (target_account_id) REFERENCES tb_account (id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
 );
 
 CREATE TABLE tb_pix (
@@ -94,6 +122,8 @@ CREATE TABLE tb_pix (
     
     CONSTRAINT FK_pix_transaction
     FOREIGN KEY (id) REFERENCES tb_transaction(id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
 );
 
 CREATE TABLE tb_ticket (
@@ -103,8 +133,6 @@ CREATE TABLE tb_ticket (
     
     CONSTRAINT FK_ticket_transaction
     FOREIGN KEY (id) REFERENCES tb_transaction(id)
+		ON UPDATE CASCADE  
+        ON DELETE RESTRICT
 );
-
-
-
-
